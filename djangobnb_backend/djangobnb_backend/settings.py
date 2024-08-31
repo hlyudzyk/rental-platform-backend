1c2d49ab5aa847ca614afeebde793ff057883c40
hlyudzyk <valerijglud0@gmail.com>
2024-08-31 06:59:38 +0300

    Added allowed hosts for linux env in settings.py

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -66,17 +66,23 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
+    'http://0.0.0.0:3000',
+    'http://0.0.0.0:8000',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
+    'http://0.0.0.0:3000',
+    'http://0.0.0.0:8000',
     'http://localhost:3000',
 ]
 
 CORS_ORIGINS_WHITELIST = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
+    'http://0.0.0.0:3000',
+    'http://0.0.0.0:8000',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
